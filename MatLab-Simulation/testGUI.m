368422679096881ac1f0ba473465690ba3956d58
ChaoLyuU <chaolyu14@gmail.com>
2016-02-15 17:13:50 +0000

    delete unnecessary function(pop up menu)

--- MatLab-Simulation/testGUI.m
+++ MatLab-Simulation/testGUI.m
@@ -22,7 +22,7 @@ function varargout = testGUI(varargin)
 
 % Edit the above text to modify the response to help testGUI
 
-% Last Modified by GUIDE v2.5 04-Feb-2016 15:35:43
+% Last Modified by GUIDE v2.5 15-Feb-2016 17:11:51
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -81,30 +81,6 @@ function varargout = testGUI_OutputFcn(hObject, eventdata, handles)
 varargout{2} = handles.output;
 
 
-
-% --- Executes on selection change in popupmenu1.
-function popupmenu1_Callback(hObject, eventdata, handles)
-% hObject    handle to popupmenu1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from popupmenu1
-
-
-% --- Executes during object creation, after setting all properties.
-function popupmenu1_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to popupmenu1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
 % --- Executes on button press in Start_Recording.
 function Start_Recording_Callback(hObject, eventdata, handles)
 % hObject    handle to Start_Recording (see GCBO)
@@ -150,28 +126,6 @@ function Playback_Callback(hObject, eventdata, handles)
 
 
 
-% --- Executes on selection change in popupmenu3.
-function popupmenu3_Callback(hObject, eventdata, handles)
-% hObject    handle to popupmenu3 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu3 contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from popupmenu3
-
-
-% --- Executes during object creation, after setting all properties.
-function popupmenu3_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to popupmenu3 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
 
 function audioTimer(hObject,varargin)
 % get the handle to the figure/GUI  (this is the handle we passed in 
@@ -181,4 +135,3 @@ function audioTimer(hObject,varargin)
  handles = guidata(hFigure);
  % get the audio samples
  samples  = getaudiodata(hObject);
-
