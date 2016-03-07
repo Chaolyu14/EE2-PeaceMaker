99d626192f55a4111c1497464ea9e5ae51d87ef6
ChaoLyuU <chaolyu14@gmail.com>
2016-03-07 16:12:55 +0000

    adding FFT to the live plot

--- lms example/SignalProcessing.m
+++ lms example/SignalProcessing.m
@@ -1,12 +1,14 @@
  noise = dsp.AudioFileReader('noise.wav');
  speech = dsp.AudioFileReader('hello.mp3');
+ hfft = dsp.FFT('FFTLengthSource', 'Property', ...
+ 'FFTLength', 512);
 
  hmfw = dsp.AudioFileWriter('Speech+Noise.wav','SampleRate', speech.SampleRate);
  hmfw1 = dsp.AudioFileWriter('ClearSignal.wav','SampleRate', speech.SampleRate); 
  AP = dsp.AudioPlayer('SampleRate',speech.SampleRate, ...
 			'QueueDuration',10, ...
 			'OutputNumUnderrunSamples',true);
- lms2 = dsp.LMSFilter('Length',11, ...
+ lms = dsp.LMSFilter('Length',11, ...
    'Method','Normalized LMS',...
    'AdaptInputPort',true, ...
    'StepSizeSource','Input port', ...
@@ -32,12 +34,21 @@ hErr = dsp.SignalSink;
 %    fprintf('Audio player queue underrun by %d samples.\n'...
 %	     ,nUnderrun);
 %    end
-
-   [y(:,1), err(:,1)] = step(lms2,audio1(:,1),audio3(:,1),mu,a);
-   [y(:,2), err(:,2)] = step(lms2,audio1(:,2),audio3(:,2),mu,a);
+    
+   [y(:,1), err(:,1)] = step(lms,audio1(:,1),audio3(:,1),mu,a);
+   [y(:,2), err(:,2)] = step(lms,audio1(:,2),audio3(:,2),mu,a);
    
-   subplot(2,1,1), plot(audio1),axis([0 1024 -1 1]), title('noise + signal');
-   subplot(2,1,2),plot(err),axis([0 1024 -1 1]), title('Err');
+   fft_y1 = step(hfft, y);
+ %  fft_y1  = fft_y1';
+   fft_err1 = step(hfft, err);
+ %  fft_err1 =  fft_err1';
+   
+   
+   subplot(2,2,1), plot(audio1),axis([0 1024 -1 1]), title('noise + signal');
+   subplot(2,2,2), plot(err),axis([0 1024 -1 1]), title('Err');
+   subplot(2,2,3), stem(fft_y1), axis([0 128 -10 10]), title('fft');
+   subplot(2,2,4), stem(fft_err1),  axis([0 128 -10 10]), title('fft_err');
+   
    drawnow
    nUnderrun = step(AP,err);
    step(hmfw1, err);         % write into a wav file
